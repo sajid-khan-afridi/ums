b87328acaf366e13e2c88275656c62961a6ea7b7
Sajid Khan Afridi <69663016+sajid-khan-afridi@users.noreply.github.com>
2022-12-27 11:54:04 +0500

    a

--- main.js
+++ main.js
@@ -1,3 +1,34 @@
 "use strict";
-console.log("hello world");
-console.log("ad");
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    getName() {
+        return this.name;
+    }
+}
+class Student extends Person {
+    constructor(name, age, rollNumber) {
+        super(name, age);
+        this.courses = [];
+        this.rollNumber = rollNumber;
+    }
+    registerForCourses(courses) {
+        this.courses.push(courses);
+    }
+}
+class Instructor extends Person {
+    constructor(name, age, salary) {
+        super(name, age);
+        this.courses = [];
+        this.salary = salary;
+    }
+    assignCourse(course) {
+        this.courses.push(course);
+    }
+}
+const sObj1 = new Student("Ali", "22", "1");
+sObj1.registerForCourses("Metaverse");
+sObj1.registerForCourses("Block Chain");
+console.log(sObj1);
--- main.ts
+++ main.ts
@@ -1,2 +1,39 @@
-console.log("hello world");
-console.log("ad");
+class Person{
+    name:string;
+    age:string;
+    constructor(name:string,age:string){
+        this.name=name;
+        this.age=age;
+
+    }
+    getName(){
+        return this.name
+    }
+}
+class Student extends Person{
+    rollNumber:string;
+    courses:string[]=[];
+    constructor(name:string,age:string,rollNumber:string){
+        super(name,age);
+        this.rollNumber=rollNumber;
+    }
+    registerForCourses(courses:string){
+        this.courses.push(courses)
+    }
+}
+
+class Instructor extends Person{
+    salary:number;
+    courses:string[]=[];
+    constructor(name:string,age:string,salary:number){
+        super(name,age);
+        this.salary=salary;
+    }
+    assignCourse(course:string){
+        this.courses.push(course);
+    }
+}
+const sObj1=new Student("Ali","22","1");
+sObj1.registerForCourses("Metaverse");
+sObj1.registerForCourses("Block Chain");
+console.log(sObj1);
